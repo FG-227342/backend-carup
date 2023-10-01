f9c1f85eff74a49d78e16c85d84f6328b22ab299
Federico Guzzo <federixo_fgm@hotmail.com>
2023-10-01 12:43:51 -0300

    primer mockup de la app

---
+++ src/main/java/com/nero/carupapi/CarupApiApplication.java
@@ -0,0 +1,13 @@
+package com.nero.carupapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarupApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarupApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nero/carupapi/CarupApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nero.carupapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarupApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
